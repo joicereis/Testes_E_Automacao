08d6f7e3f115c00e102e585eb16b0fa099792ad1
joice.jdosreis@gmail.com <joice.jdosreis@gmail.com>
2024-12-28 16:25:29 -0300

    Implantação de método de exclusão de transações criadas

--- agilizei-cypress/cypress/e2e/finances.cy.js
+++ agilizei-cypress/cypress/e2e/finances.cy.js
@@ -11,13 +11,38 @@ describe('Transações', () => {
     });
 
     it('Cadastrar uma entrada', () => {
-        criarTransacao("Frelancer Programação", "250.00");
-        cy.get("tbody tr td.description").should("have.text", "Frelancer Programação");
+        criarTransacao("Frelancer", "250.00");
+        cy.get('tbody tr td.description').should("have.text", "Frelancer");
     });
 
     it('Cadastrar uma saída', () => {
         criarTransacao("Cinema", "-65.59");
-        cy.get("tbody tr td.description").should("have.text", "Cinema");
+        cy.get('tbody tr td.description').should("have.text", "Cinema");
+    });
+
+    it.only('Excluir transação', () => {
+        //deve se criar a linha nesse teste para que seja possível localizá-la
+        criarTransacao("Frelancer", "250.00");
+        criarTransacao("PetSitter", "40.00");
+
+        /*
+        //depois de criada a linha, é possível encontrá-la através de um seletor que nesse caso foi o .description que possua o texto "Frelancer"
+        cy.contains(".description", "Frelancer")
+            .parent() //método que permite encontrar o elemento pai do elemento localizado anteriormente
+            .find('img').click(); ////método que permite buscar por um emelento 'img'
+
+        cy.get('tbody tr').should("have.length", 1);
+        cy.get('tbody tr td.description').should("have.text", "PetSitter");
+        */
+
+        //OUTRA FORMA DE LOCALIZAR SERIA PELOS ELEMENTOS DE MESMO NÍVEL
+        cy.contains(".description", "Frelancer")
+            .siblings() //encontra elementos de mesmo nível
+            .children('img') //dentro desses elementos de mesmo nível, vai buscar pelo "filho" 'img'
+            .click();
+
+        cy.get('tbody tr').should("have.length", 1);
+        cy.get('tbody tr td.description').should("have.text", "PetSitter");
     });
 });
 
